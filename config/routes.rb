bad68da6c457f3cab05dfd538928bb57318d5159
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-02-08 02:58:40 +0900

    komito04

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'gameposts/index'
+  get 'gameposts/new'
+  get 'gameposts/create'
+  get 'gameposts/show'
+  get 'gameposts/destroy'
+  get 'gameposts/edit'
+  get 'gameposts/update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
