f2596b48f04c2e5e6fb78f65c897e1bc0da2f602
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-02-16 23:21:37 +0900

    komito12

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root 'gameposts#index'
+  get 'gameposts' =>  'gameposts#index'
+  get 'gameposts/new' => 'gameposts#new'
+  post 'gameposts' => 'gameposts#create'
+  get 'users/:id' => 'users#show'
+  get 'gameposts/:id' => 'gameposts#show'
+  get 'gameposts/:id/destroy' => 'gameposts#destroy'
+  get '/gameposts/gameposts/:id/edit' => 'gameposts#edit'
+  patch 'gameposts/:id' => 'gameposts#update'
 end
