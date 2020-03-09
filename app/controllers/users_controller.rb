7805e2e8977f581c201edb5be1df7306527556e9
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-03-09 10:45:19 +0900

    komito20

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    user = User.find(params[:id])
-    @nickname = user.nickname
+    @games = Game.where(user_id: current_user.id).page(params[:page]).per(50).order("created_at DESC")
+    @nickname = current_user.nickname
   end
 end
