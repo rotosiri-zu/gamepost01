3523f7607a241af16ea28799a2b5bdb388d0b276
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-03-09 19:33:43 +0900

    投稿一覧の作成

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   get 'gameposts/:id/destroy' => 'gameposts#destroy'
   get '/gameposts/gameposts/:id/edit' => 'gameposts#edit'
   patch 'gameposts/:id' => 'gameposts#update'
+  get 'users/users/:id' => 'users#listofposts'
 end
