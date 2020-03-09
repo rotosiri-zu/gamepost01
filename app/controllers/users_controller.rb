bdec6fed47ebe7384eec5ba5b3a02b8c5226c7a3
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-03-09 21:35:20 +0900

    不要なコードを削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   def show
-    @games = Game.where(user_id: current_user.id).page(params[:page]).per(50).order("created_at DESC")
     @nickname = current_user.nickname
   end
 
