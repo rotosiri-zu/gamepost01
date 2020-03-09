683c2c18b5e8a8ac82469e48da151e129ae8ec3f
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-03-09 21:33:56 +0900

    投稿一覧のビュー作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,9 @@ class UsersController < ApplicationController
     @games = Game.where(user_id: current_user.id).page(params[:page]).per(50).order("created_at DESC")
     @nickname = current_user.nickname
   end
+
+  def listofposts
+    @games = Game.where(user_id: current_user.id).page(params[:page]).per(50).order("created_at DESC")
+    @nickname = current_user.nickname
+  end  
 end
