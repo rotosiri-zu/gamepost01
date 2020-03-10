d0ecbeac1371ea1485a59d11a3fe9963346f99f7
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-03-10 19:30:58 +0900

    検索機能の追加修正版

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     before_action :configure_permitted_parameters, if: :devise_controller?
     before_action :set_current_user
+    before_action :set_search
     
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
@@ -11,5 +12,10 @@ class ApplicationController < ActionController::Base
 
     def set_current_user
       # @current_user = User.find(params[:id])
+    end
+
+    def set_search
+      @search = Game.ransack(params[:q])
+      @search_products = @search.result.page(params[:page])
     end
 end
