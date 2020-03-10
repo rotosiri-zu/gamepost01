1567c7f624845da1a4223a15c6800d23ecd8f763
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-03-11 01:12:35 +0900

    ルーティングをネスト

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
-  resources :gameposts, only: [:index, :show, :new, :create, :edit, :update, :destroy] 
+  resources :gameposts, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+    resources :reviews, only: [:new, :create]
+  end  
   root 'gameposts#index'
   resources :users, only: [:show]
   get 'users/users/:id' => 'users#listofposts'
-  resources :reviews, only: [:new, :create]
 end
