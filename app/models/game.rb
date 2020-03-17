b525135fd0aa794bce5dd5038f74b36ebedc95cd
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-03-17 18:52:15 +0900

    komito19

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,6 @@
 class Game < ApplicationRecord
   mount_uploader :image, ImagesUploader
-  
   belongs_to :user
+  has_many :review
+  validates :image, :name, :platform, :genre, :text, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'gameposts#index'
-  get 'gameposts' =>  'gameposts#index'
-  get 'gameposts/new' => 'gameposts#new'
-  post 'gameposts' => 'gameposts#create'
-  get 'users/:id' => 'users#show'
-  get 'gameposts/:id' => 'gameposts#show'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  resources :gameposts
   get 'gameposts/:id/destroy' => 'gameposts#destroy'
-  get '/gameposts/gameposts/:id/edit' => 'gameposts#edit'
-  patch 'gameposts/:id' => 'gameposts#update'
+  resources :users
+  root to: 'gameposts#index'
 end
