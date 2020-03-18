f0bcc6cdd491dc6bf9ef29c8a668587084319642
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-03-18 20:56:23 +0900

    検索してきた物を表示できるように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
     before_action :set_current_user
     before_action :set_search
+    
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
     end
