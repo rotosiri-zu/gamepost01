683bac667afa8f560fb3c79d5a4fd5df6463490c
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-03-27 06:42:35 +0900

    reviewテストコードカラム追加

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :review do
-    
+    nickname                           {""}
+    rete                               {1}
+    review                             {"テスト"}
+    product_id                         {1}
   end
 end
