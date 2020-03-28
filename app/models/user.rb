5a0c21b3eede1c0ef8c99e6da9e4e2dc1ec2b026
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-03-28 20:03:56 +0900

    game,userモデルのテストコード、reviewモデルのテストコード(途中)を記入

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :game
   has_many :review
+  validates :nickname, :email, :password, :password_confirmation, presence: true
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "有効なファクトリをもつ" do
+    expect(FactoryBot.build(:review)).to be_valid
+  end
+  
 end
