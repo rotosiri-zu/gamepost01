5385fee6fd1f8d48a4c45d38749c1305f6065308
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-04-03 00:04:38 +0900

    機能テスト(request spec)の追加

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -1,10 +1,21 @@
 FactoryBot.define do
   factory :game do
+    id                    {1} 
     name                  {"テスト"}
     image                 { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/618YoWFaSiL.jpg'))}
     text                  {"サンプル"}
     platform              {"ps4"}
     genre                 {"rpg"}
     user_id               {1}
+  end
+
+  trait :invalid do
+    id                    {''} 
+    name                  {''}
+    image                 {''}
+    text                  {''}
+    platform              {''}
+    genre                 {''}
+    user_id               {''}
   end
 end
