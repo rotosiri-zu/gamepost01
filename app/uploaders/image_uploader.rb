f6b66e49ce71c68eac928722a32d730936c49616
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-04-04 10:11:58 +0900

    S3設定変更

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,11 +3,14 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
 
+  process resize_to_fill: [800, 800]
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
-  storage :fog
   
+    storage :fog
+    # storage :file
+
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
