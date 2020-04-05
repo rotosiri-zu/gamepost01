ec470d2228141f56ec7aedebc43b6e5c7f042bf0
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-04-05 17:29:30 +0900

    テストコードコメントアウト解除

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,9 +1,9 @@
-# FactoryBot.define do
-#   factory :review do
-#     id                                 {1}
-#     nickname                           {"テスト"}
-#     rate                               {1}
-#     review                             {"テスト"}
-#     product_id                         {1}
-#   end
-# end
+FactoryBot.define do
+  factory :review do
+    id                                 {1}
+    nickname                           {"テスト"}
+    rate                               {1}
+    review                             {"テスト"}
+    product_id                         {1}
+  end
+end
