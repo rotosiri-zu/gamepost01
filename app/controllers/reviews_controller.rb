4bb5cc5411dd9ac78656445a2a74a0ec3ea2bb10
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-04-06 21:10:43 +0900

    komito36

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,15 @@
 class ReviewsController < ApplicationController
+  def new
+    @reviews = Review.new
+  end
+  
+  def create
+    @product = Review.create(create_params)
+    redirect_to controller: :games, action: :index
+  end
+
+  private
+  def create_params
+    params.require(:review).permit(:nickname, :rate, :review).merge(product_id: params[:product_id])
+  end
 end
