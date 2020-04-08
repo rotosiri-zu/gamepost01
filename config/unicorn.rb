f58da800788cfe3bc88d4e5334119bc487afe4ff
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-04-08 20:26:26 +0900

    設定変更

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,14 +2,13 @@ app_path = File.expand_path('../../', __FILE__)
 
 worker_processes 1
 
-working_directory app_path
+working_directory "#{app_path}"
 listen "#{app_path}/tmp/sockets/unicorn.sock"
 pid "#{app_path}/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/log/unicorn.stderr.log"
 stdout_path "#{app_path}/log/unicorn.stdout.log"
 
 timeout 60
-
 
 preload_app true
 GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
