7df1a6e84a4f5e0515e5b4fc05f92393fe0559ba
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-04-10 11:23:21 +0900

    komito37

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_06_083721) do
+ActiveRecord::Schema.define(version: 2020_04_03_193839) do
 
   create_table "games", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
