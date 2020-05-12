db95e9bb3817bed54d37a419adf61c9d2ae20c26
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-05-12 11:35:12 +0900

    komito44

--- app/controllers/socialgame_controller.rb
+++ app/controllers/socialgame_controller.rb
@@ -1,4 +1,5 @@
 class SocialgameController < ApplicationController
+  
   def index
   end
 
--- app/controllers/videogames_controller.rb
+++ app/controllers/videogames_controller.rb
@@ -1,5 +1,8 @@
 class VideogamesController < ApplicationController
+
   def index
+    @games = Game.all
+    @games = Game.order("created_at DESC").limit(26)
   end
 
   def show
