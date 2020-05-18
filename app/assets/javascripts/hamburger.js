c052af0462aa142525db51ad2a70d7518ed68e30
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-05-18 18:46:06 +0900

    ハンバーガーメニュー修正

--- app/assets/javascripts/hamburger.js
+++ app/assets/javascripts/hamburger.js
@@ -1,39 +1,5 @@
-$(function() {
-
-  const btn = $('#hamburger_icon');
-  const win = $("#drawer_background");
-  const nav = $("nav.menuitems");
-
-  function toggleMenu() {
-    if (btn.hasClass("open")) {
-      closeMenu();
-    } else {
-      openMenu();
-    }
-  }
-
-  function openMenu() {
-    btn.addClass("open");
-    execMenu(0);
-    win.css('right', '0px');
-    win.css('top', '0px');
-    win.css('height', '100%');
-    win.css('width', '100%');
-    win.fadeIn();
-  }
-
-  function closeMenu() {
-    btn.removeClass("open");
-    execMenu(-300);
-    win.fadeOut();
-  }
-
-  function execMenu(pos) {
-    nav.stop().animate({
-      left: pos
-    }, 200);
-  }
-  btn.on("click", toggleMenu);
-  win.on("click", closeMenu);
-  
-});
+$(function(){
+  $('.nav_toggle').on('click', function () {
+      $('.nav_toggle, .nav').toggleClass('show');
+  });
+})
