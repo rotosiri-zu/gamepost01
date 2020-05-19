cda98670b947f2447da42bd63a46f00eb4c8a037
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2020-05-19 14:33:48 +0900

    bootstrapの導入

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,8 @@
 // about supported directives.
 //
 //= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 //= require jquery_ujs
 //= require jquery.raty
 //= require rails-ujs
